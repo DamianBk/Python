84b499ef9d2e9dcc9c73ed80f95a7818b1b0dbbe
DamianBk <128623958+DamianBk@users.noreply.github.com>
2023-03-23 18:21:59 +0100

    Update bubble_sort.py

--- Algorithms/Sorting algorithms/bubble_sort.py
+++ Algorithms/Sorting algorithms/bubble_sort.py
@@ -1,17 +1,14 @@
 """Algorithm bubble sort"""
 
-
 def bubble_sort(sequence: list):
     seq_len = len(sequence)
-
     for i in range(seq_len):
         for j in range(seq_len - 1):
-            if sequence[i] < sequence[j]:
-                sequence[i], sequence[j] = sequence[j], sequence[i]
+            if sequence[j] > sequence[j + 1]:
+                sequence[j], sequence[j + 1] = sequence[j + 1], sequence[j]
     return sequence
 
-
-unsorted = [2, 1, 3, 6, 4, 7, 5]
+unsorted = [9, 0, 2, 1, 3, 6, 4, 7, 5]
 
 bubble_sort(unsorted)
 
